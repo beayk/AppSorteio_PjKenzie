c132d23a65e6fe822b50ddc02f51b170531be777
beayk <dlbeatriz0@gmail.com>
2022-04-05 12:58:41 -0300

    Projeto Kenzie

---
+++ Kenzie_Project/assets/js/script.js
@@ -0,0 +1,43 @@
+// INTERCEPTAR O CLIQUE NO BOTÃO
+// PEGAR AS INFORMAÇÕES QUE ESTÃO DENTRO DO CAMPO
+
+const btnSortear        = document.querySelector(".botaoSortear")
+const textarea          = document.querySelector("textarea")
+const tagResultado      = document.querySelector(".resultado p")
+const popUpResultado    = document.querySelector(".resultado")
+const btnFechar         = document.querySelector(".fechar")
+
+// PEGANDO DADOS DO FORMULARIO
+
+function pegarDadosFormulario(){
+    const valorCampo    = textarea.value
+    const listaNomes    = valorCampo.split(",")
+    const tamanhoArray  = listaNomes.length
+    const posicaoNome   = gerarNumeroAleatorio(tamanhoArray)
+    const nome          = listaNomes[posicaoNome]
+    mostrarResultado(nome)
+
+    console.log(listaNomes[posicaoNome])
+}
+// INTERCEPTANDO O CLIQUE / EXECUTANDO FUNÇÃO
+btnSortear.addEventListener("click", pegarDadosFormulario)
+
+// GERANDO  NÚMERO ALEATÓRIO
+function gerarNumeroAleatorio(tamanhoArray){
+    // RECEBENDO NÚMERO
+    return Math.floor(Math.random() * (tamanhoArray - 0) + 0)
+}
+
+function mostrarResultado(nome){
+
+    tagResultado.innerText = `Resultado: ${nome}`
+    popUpResultado.classList.add("mostrarResultado")
+
+}
+
+function fecharPopUp(){
+
+    popUpResultado.classList.remove("mostrarResultado")
+
+}    
+btnFechar.addEventListener("click", fecharPopUp)
